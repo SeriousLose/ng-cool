4f8d4fa68d0c951d52e7554885d8171c8c005480
helongfei <helongfei>
2019-05-27 22:34:38 +0800

    <feature> i18n练习

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.less"]
 })
 export class AppComponent {
-  title = 'app';
+  title = "app";
+
+  constructor(private translate: TranslateService) {}
+
+  public async ngOnInit() {
+    // 语言初始化(若未设置语言, 则取浏览器语言)
+    let currentLanguage = (await localStorage.getItem("currentLanguage")) || this.translate.getBrowserCultureLang();
+    // 当在assets/i18n中找不到对应的语言翻译时，使用'zh-CN'作为默认语言
+    this.translate.setDefaultLang("zh-CN");
+    this.translate.use(currentLanguage);
+    // 记录当前设置的语言
+    localStorage.setItem("currentLanguage", currentLanguage);
+  }
+
+  public selectLanguage(lang) {
+    this.translate.use(lang);
+    // 更新当前记录的语言
+    localStorage.setItem('currentLanguage', lang)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+import { HttpClientModule, HttpClient } from "@angular/common/http";
+import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
