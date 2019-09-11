814e591b4d90a14ac14ffe6232b9820cfb3210e5
helongfei <helongfei>
2019-09-11 18:43:16 +0800

    <text> 测试

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
-import { HttpClientModule, HttpClient } from "@angular/common/http";
 import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
 import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 import { DebounceClickDirective } from "./framework/directive/debounce-click.directive"; // 防止连点
 import { UserHttpService } from "./framework/http/http";
 import { ApiRequestService } from "./framework/services/api.server";
+import { HttpClient, HttpClientModule } from "../../node_modules/@angular/common/http";
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
